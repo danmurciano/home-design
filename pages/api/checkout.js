df339d42e538ded57ab7f45c71ad24bfe44a35d4
Dan Murciano <danmurciano@dans-mac-pro.home>
2021-02-04 22:54:04 -0800

    version 1

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
-import uuidv4 from "uuid/v4";
+import uuidv4 from "uuidv4";
 import jwt from "jsonwebtoken";
 import Cart from "../../models/Cart";
 import User from "../../models/User";
