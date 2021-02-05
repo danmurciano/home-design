058eb625a3535720b2d63159a640e9ab254b2a4f
Dan Murciano <danmurciano@dans-mac-pro.home>
2021-02-04 21:21:26 -0800

    version 1

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://deployment-url.now.sh"
+    ? "https://home-design-l93els9ff.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
