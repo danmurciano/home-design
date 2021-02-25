e27c3bd174638b8066ea8cf21fcd2c2cddc6997a
Dan Murciano <danmurciano@dans-mac-pro.home>
2021-02-24 21:19:46 -0800

    version 1

--- pages/_app.js
+++ pages/_app.js
@@ -98,20 +98,6 @@ class MyApp extends App {
     }
   }
 
-  async headers() {
-  return [
-    {
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-      ]
-    }
-  ]
-}
-
 
   render() {
     const { Component, pageProps } = this.props;
--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://home-design.danmurciano.vercel.app"
+    ? "https://home-design-danmurciano.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
